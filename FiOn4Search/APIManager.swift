0cf391a11b31a8ca8c531b7f7c2a1b706ebf4482
zoop1259 <kdm432191@gmail.com>
2022-08-19 01:00:07 +0900

    옵셔널 값이지만 일단 완료.

--- FiOn4Search/APIManager.swift
+++ FiOn4Search/APIManager.swift
@@ -67,9 +67,98 @@ extension API {
         }
     }
     
+    //API 적용 여부 --> 이 프로젝트에선 모두 사용해야하지만 나중에 사용하지 않는 프로젝트를 위해
+    private var isNeedAPIKey: Bool {
+        switch self {
+        case .getAccessId, .getTier, .getMatchId, .getMatchInfo:
+            return true
+        default: //디폴트는 없지만.. 그래도 형식상 씀....나중에 사용하지 않는 api를 위해
+            return false
+        }
+    }
     
+    //통신 헤더
+    private var header: HTTPHeaders {
+
+        var result = HTTPHeaders([HTTPHeader(name: "Authorization", value: authorization)])
+        
+//        if isNeedAPIKey {
+//            var result = HTTPHeader(name:"Authorization", value: authorization)
+//        }
+        return result
+    }
+    
+    //Body 파라미터인데 Get으로 예상되어 필요 x
+    private var parameter: [String:Any]? {
+        switch self {
+        case .getAccessId, .getTier, .getMatchId, .getMatchInfo: return nil
+        }
+    }
+
+    func request(complete: @escaping ((Result<NSDictionary?,CustomError>)->())) {
+        var url = domain
+        
+        let headers: HTTPHeaders = [.authorization("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE0MDkzMDI3MDAiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY3NDg5NjIxMCwiaWF0IjoxNjU5MzQ0MjEwLCJuYmYiOjE2NTkzNDQyMTAsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.nwgL3AMU216uu88opO2R4br3uMRE1_86V9w0Uh7TbN0")]
+
+        //addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed
+        if let convertedPath = path.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) { //.urlPathAllowed 원래는 이거였다
+            url += convertedPath
+        }
+        AF.request(url, method: method, parameters: parameter, headers: headers)
+            .responseJSON { response in
+                //print(url) //여기서 찾아낸 url에러!
+                //print(response)
+                // 통신 결과 처리
+                switch response.result {
+                case .success(let dict):
+                    // 성공
+                    complete(.success(dict as? NSDictionary))
+                case .failure:
+                    
+                    // 결과는 없지만 statusCode에 따라 확인이 필요할 수 있음.
+                    if let stCode = response.response?.statusCode, 200..<300 ~= stCode {
+                        // 성공
+                        complete(.success(nil))
+                    } else {
+                        // 진짜 실패
+                        complete(.failure(.invalidState))
+                    }
+                }
+            }
+    }
+    
+//    func apirequest<T: Decodable>(dataType: T.Type, complete: @escaping ((Result<T,CustomError>)->())) {
+//
+//        // 1. 위에 선언한 요청 메소드를 통해 NSDictionary를 받는다.
+//        apirequest(dataType: T.self) { result in
+//            switch result {
+//            case .success(let dict):
+//
+//                // 2. 데이터 존재 확인
+//    //                guard let dicData = dict else {
+//    //                    // 데이터 미존재 에러
+//    //                    complete(.failure(.invalidData))
+//    //                    return
+//    //                }
+//
+//                // 3. 얻은 NSDictionary를 JSON 데이터로 바꾼뒤 T형태로 Decode 해준다.
+//                guard
+//                    let json = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted),
+//                    let data = try? JSONDecoder().decode(T.self, from: json)
+//                else {
+//                    // 4. Decode실패
+//                    complete(.failure(.invalidState))
+//                    return
+//                }
+//
+//                // 5. T 객체 생성 성공!
+//                complete(.success(data))
+//
+//            case .failure(let e):
+//                // 6. Request 실패
+//                complete(.failure(e))
+//            }
+//        }
+//    }
     
 }
-
-
-let headers: HTTPHeaders = [.authorization("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJYLUFwcC1SYXRlLUxpbWl0IjoiNTAwOjEwIiwiYWNjb3VudF9pZCI6IjE0MDkzMDI3MDAiLCJhdXRoX2lkIjoiMiIsImV4cCI6MTY3NDg5NjIxMCwiaWF0IjoxNjU5MzQ0MjEwLCJuYmYiOjE2NTkzNDQyMTAsInNlcnZpY2VfaWQiOiI0MzAwMTE0ODEiLCJ0b2tlbl90eXBlIjoiQWNjZXNzVG9rZW4ifQ.nwgL3AMU216uu88opO2R4br3uMRE1_86V9w0Uh7TbN0")]
--- FiOn4Search/SearchController.swift
+++ FiOn4Search/SearchController.swift
@@ -138,7 +138,8 @@ class SearchController: UIViewController {
         searchBtn.rx.tap
             .subscribe(onNext: {_ in
                 //getUserId()
-                self.MaingetUserId()
+                //self.MaingetUserId()
+                self.getRequest()
             }).disposed(by: bag)
         
 //        nameLabel.rx.observe(String.self, "text")
@@ -408,6 +409,72 @@ class SearchController: UIViewController {
             }
         //여기선 데이터 증발
     }
+    
+    
+    //MARK: - 제네릭 클로저
+    /// 통신 요청 (with Generic)
+    /// - Parameters:
+    ///     - dataType: Generic으로 선언된 자료형의 타입을 받는다.
+    ///     - complete: 클로저 - 성공시 T의 객체, 실패시 선언해둔 에러
+    func request<T: Decodable>(dataType: T.Type, complete: @escaping ((Result<T,CustomError>)->())) {
+        
+        // 1. 위에 선언한 요청 메소드를 통해 NSDictionary를 받는다.
+        request(dataType: T.self) { result in
+            switch result {
+            case .success(let dict):
+                
+                // 2. 데이터 존재 확인
+//                guard let dicData = dict else {
+//                    // 데이터 미존재 에러
+//                    complete(.failure(.invalidData))
+//                    return
+//                }
+                
+                // 3. 얻은 NSDictionary를 JSON 데이터로 바꾼뒤 T형태로 Decode 해준다.
+                guard
+                    let json = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted),
+                    let data = try? JSONDecoder().decode(T.self, from: json)
+                else {
+                    // 4. Decode실패
+                    complete(.failure(.invalidState))
+                    return
+                }
+                
+                // 5. T 객체 생성 성공!
+                complete(.success(data))
+                
+            case .failure(let e):
+                // 6. Request 실패
+                complete(.failure(e))
+            }
+        }
+    }
+    
+    
+    func getRequest() {
+        let api = API.getAccessId(name: self.userNickName)
+        api.request { result in
+            print(result)
+            switch result {
+            case .success(let dict):
+                print(dict)
+                
+            case .failure(let e):
+                print(e)
+            }
+        }
+    }
+        
+        
+//        api.request(dataType: UserInfo.self) { result in
+//            switch result {
+//            case .success(let data):
+//                print(data)
+//            case .failure(let error):
+//                print(error)
+//            }
+//        }
+    
 }
 
 
